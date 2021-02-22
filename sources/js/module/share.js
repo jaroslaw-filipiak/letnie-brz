c7a6a6d96cdd02e992df615e5a95381d7475ba43
jaroslaw-filipiak <info@j-filipiak.pl>
2021-02-22 14:27:10 +0100

    first look

--- sources/js/module/share.js
+++ sources/js/module/share.js
@@ -1,1 +1,8 @@
 console.log('shared between all pages / subpages')
+
+const nav = document.querySelector('.nav-right') ,
+      hamburger = document.querySelector('.hamburger')
+
+      hamburger.onclick = function () {  
+          nav.classList.toggle('nav-right__open');
+      };
