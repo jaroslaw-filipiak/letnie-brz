05e88680fd88773365b4af60b5942a5272444c19
jaroslaw-filipiak <info@j-filipiak.pl>
2021-02-24 09:32:56 +0100

    preloader

--- sources/js/module/share.js
+++ sources/js/module/share.js
@@ -2,9 +2,15 @@ console.log('shared between all pages / subpages')
 
 const nav = document.querySelector('.nav-right') ,
       hamburger = document.querySelector('.hamburger'),
-      cross = document.querySelector('.hamburger--spin')
+      cross = document.querySelector('.hamburger--spin'),
+      preloader = document.querySelector('.preloader')
 
       hamburger.onclick = function () {  
           nav.classList.toggle('nav-right__open');
           cross.classList.toggle('is-active')
       };
+
+
+      window.addEventListener('load', () => {
+        preloader.classList.add('preloader__hide')
+    });
